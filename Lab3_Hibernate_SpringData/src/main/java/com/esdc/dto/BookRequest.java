98e07610994203d5b6d36fa097bb884e27b73987
baseptr <zoltan.vargo@gmail.com>
2026-05-11 19:23:00 +0300

    fix: notNull

--- Lab3_Hibernate_SpringData/src/main/java/com/esdc/dto/BookRequest.java
+++ Lab3_Hibernate_SpringData/src/main/java/com/esdc/dto/BookRequest.java
@@ -10,7 +10,7 @@ import jakarta.validation.constraints.Positive;
 
 public record BookRequest(@NotBlank String title,
                           @NotBlank String author,
-                          @ValidIsbn @Schema(description = "ISBN-13: 13 цифр, дефисы допустимы", example = "978-0-13-235088-4") String isbn,
+                          @ValidIsbn @Schema(description = "ISBN-13: 13 цифр, дефисы допустимы", example = "978-0-13-235088-4") @NotNull String isbn,
                           @Min(1) Double price,
                           @NotBlank String genre,
                           @Max(2026) @Min(1000) Integer publishYear,
